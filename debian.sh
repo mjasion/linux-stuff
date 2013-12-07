7df43a2b15101c766e08218ce8aa3632f8334b11
Marcin Jasion <marcin.jasion@gmail.com>
2013-12-07 10:12:33 +0100

    added repo files for debian and ubuntu

--- debian.sh
+++ debian.sh
@@ -1,48 +1,23 @@
 #!/bin/sh
 # Marcin Jasion
 
+OS=$(lsb_release -si)
+
 if [ $USER != 'root' ]; then
         echo "Sorry, you need to run this as root"
         exit
 fi
 
-apt-get install python-software-properties
+apt-get install python-software-properties -y
 
-# oracle java
-echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" >> /etc/apt/sources.list.d/oracle-java.list
-echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu precise main" >> /etc/apt/sources.list.d/oracle-java.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
-
-# mariadb
-echo "deb http://mariadb.mirror.nucleus.be//repo/10.0/debian wheezy main" >> /etc/apt/sources.list.d/mariadb.list
-apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
-
-# postgresql
-echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" >> /etc/apt/sources.list.d/postgresql.list
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-
-# jenkins
-echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.list.d/jenkins.list
-wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
-
-# mongodb
-echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' >> /etc/apt/sources.list.d/mongodb.list
-sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
-
-# fish shell
-echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_7.0/ ./' >> /etc/apt/sources.list.d/fish-shell.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-key D880C8E4
-
-# node.js
-echo "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu raring main " >> /etc/apt/sources.list.d/nodejs.list
-echo "deb-src http://ppa.launchpad.net/chris-lea/node.js/ubuntu raring main" >> /etc/apt/sources.list.d/nodejs.list
-apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C7917B12
-
-# git
-echo "deb http://ppa.launchpad.net/git-core/ppa/ubuntu saucy main" >> /etc/apt/sources.list.d/git.list
-echo "deb-src http://ppa.launchpad.net/git-core/ppa/ubuntu saucy main" >> /etc/apt/sources.list.d/git.list
+if [ "Debian" = "$OS" ]
+then
+	wget https://raw.github.com/mjasion/linux-stuff/master/repo/debian-repo.sh -q -O - | sudo sh
+else
+	wget https://raw.github.com/mjasion/linux-stuff/master/repo/ubuntu-repo.sh -q -O - | sudo sh
+fi
 
 apt-get update
-apt-get install ntp vim nmap postgresql-9.3 mariadb-server mongodb-10gen tcpdump oracle-jdk7-installer dnsutils fish postfix nginx-extras openvpn htop iotop bwm-ng git gitk subversion nodejs jenkins maven
+apt-get install ntp vim nmap postgresql-9.3 mariadb-server mongodb-10gen tcpdump oracle-jdk7-installer dnsutils fish postfix nginx-extras openvpn htop iotop bwm-ng git gitk subversion nodejs jenkins maven -y
 apt-get purge nano 
 apt-get purge openjdk*
---
+++ repo/debian-repo.sh
@@ -0,0 +1,45 @@
+#!/bin/sh
+# Marcin Jasion
+
+# oracle java
+rm /etc/apt/sources.list.d/oracle-java.list
+echo "deb http://ppa.launchpad.net/webupd8team/java/ubuntu saucy main" >> /etc/apt/sources.list.d/oracle-java.list
+echo "deb-src http://ppa.launchpad.net/webupd8team/java/ubuntu saucy main" >> /etc/apt/sources.list.d/oracle-java.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys EEA14886
+
+# mariadb
+rm /etc/apt/sources.list.d/mariadb.list
+echo "deb http://mariadb.cu.be//repo/10.0/debian wheezy main" >> /etc/apt/sources.list.d/mariadb.list
+apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 0xcbcb082a1bb943db
+
+# postgresql
+rm /etc/apt/sources.list.d/postgresql.list
+echo "deb http://apt.postgresql.org/pub/repos/apt/ wheezy-pgdg main" >> /etc/apt/sources.list.d/postgresql.list
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+# jenkins
+rm /etc/apt/sources.list.d/jenkins.list
+echo "deb http://pkg.jenkins-ci.org/debian binary/" >> /etc/apt/sources.list.d/jenkins.list
+wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+
+# mongodb
+rm /etc/apt/sources.list.d/mongodb.list
+echo 'deb http://downloads-distro.mongodb.org/repo/debian-sysvinit dist 10gen' >> /etc/apt/sources.list.d/mongodb.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv 7F0CEB10
+
+# fish shell
+rm /etc/apt/sources.list.d/fish-shell.list
+echo 'deb http://download.opensuse.org/repositories/shells:/fish:/release:/2/Debian_7.0/ ./' >> /etc/apt/sources.list.d/fish-shell.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-key D880C8E4
+
+# node.js
+rm /etc/apt/sources.list.d/nodejs.list
+echo "deb http://ppa.launchpad.net/chris-lea/node.js/ubuntu saucy main " >> /etc/apt/sources.list.d/nodejs.list
+echo "deb-src http://ppa.launchpad.net/chris-lea/node.js/ubuntu saucy main" >> /etc/apt/sources.list.d/nodejs.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys C7917B12
+
+# git
+rm /etc/apt/sources.list.d/git.list
+echo "deb http://ppa.launchpad.net/git-core/ppa/ubuntu saucy main" >> /etc/apt/sources.list.d/git.list
+echo "deb-src http://ppa.launchpad.net/git-core/ppa/ubuntu saucy main" >> /etc/apt/sources.list.d/git.list
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E1DF1F24 
