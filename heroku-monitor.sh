0d047b7640aece67e145d9ea88d37181cd013a86
Marcin Jasion <marcin.jasion@gmail.com>
2015-04-12 14:31:56 +0200

    Update heroku-monitor.sh

--- heroku-monitor.sh
+++ heroku-monitor.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-SITES="http://obibooki.herokuapp.com/ http://astrobotpl.herokuapp.com/"
+SITES="http://obibooki.herokuapp.com/ http://astrobotpl.herokuapp.com/ http://movies-rss.herokuapp.com/admin/statistics"
 for site in $SITES; do
     wget -q -O-  $site >> /dev/null
 done
